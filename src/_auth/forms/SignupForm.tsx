209d0db05c05a2f50b413903ac9fb1395d315757
zurizheng <zurizheng88@gmail.com>
2025-03-04 23:20:57 -0400

    Implemented post details page

--- src/_auth/forms/SignupForm.tsx
+++ src/_auth/forms/SignupForm.tsx
@@ -68,7 +68,7 @@ const SignupForm = () => {
       <div className="sm:w-420 flex-center flex-col">
         <img src="/assets/images/logo.svg" alt="logo" />
         <h2 className="h3-bold md:h2-bold pt-5 sm:pt-12">Create a new account</h2>
-        <p className="text-light-3 small-medium md:base-regular mt-2">To use Munchrz, please enter your details</p>
+        <p className="text-light-3 small-medium md:base-regular mt-2">To use Munchr, please enter your details</p>
       
         <form onSubmit={form.handleSubmit(onSubmit)} className="flex flex-col gap-5 w-full mt-4">
           <FormField
